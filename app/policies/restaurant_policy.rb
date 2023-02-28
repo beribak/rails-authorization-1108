a7c4588e2faf7bc535ba5913057f07bf25985370
danko beribak <beribakdanko@gmail.com>
2023-02-28 10:01:48 +0100

    second commit

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -19,7 +19,8 @@ class RestaurantPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.all # show all restaurants
+      # scope.where(user: user)  show only the restaurants of the user
     end
   end
 end
